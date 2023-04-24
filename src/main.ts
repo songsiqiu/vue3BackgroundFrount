7efe10d098e7ae240354d1ae8c37068ada0028cc
宋梦辉 <327127079@qq.com>
2023-04-24 18:02:00 +0800

    全局样式初始化修正

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
+import App from './App.vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
 
+import router from './router'
+import '@/styles/index.scss'
 import { setupGlobComponent } from '@/components'
 
 
